e4fd1b6a7ff3561717091903fd848a9ffdecad68
thmsmlr <millar.thomas@gmail.com>
2019-06-07 16:35:22 +0000

    Add stitchfix user email

--- config.fish
+++ config.fish
@@ -6,6 +6,8 @@ set -gx LANG en_US.utf8
 set -gx PYENV_ROOT /stitchfix/z-pyenv/pyenv
 set -gx PATH $PYENV_ROOT/bin $PATH
 
+set -gx STITCHFIX_USER_EMAIL tmillar@stitchfix.com
+
 
 function activate
   source $PYENV_ROOT/versions/$argv/bin/activate.fish
